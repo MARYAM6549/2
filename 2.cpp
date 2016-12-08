f9c10e54f4f1aa784aa19620b09fd84c9fa85f75
maryam^c <maryam.abbasi.6549@gmail.com>
2016-12-08 09:32:31 +0330

    emal operator bar adad

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+using namespace std;
+int main(int argc, char const *argv[]) {
+  int number1,number2;
+  cout<<"Enter 2 integer:\n";
+   cout<<"number1=";
+   cin >> number1 ;
+   cout<<"number2=";
+   cin >> number2 ;
+   cout<<"\nsum = "<<number1+number2<<"\ndifference = "<<number1-number2<<"\nproduct = "<<number1*number2<<"\nquotient = "<<number1/number2<<"\n(number1)^2 = "<<number1*number1<<"\n(number2)^2 = "<<number2*number2<<"\n";
+//integer quotient
+  return 0;
+}
